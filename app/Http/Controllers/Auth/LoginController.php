0e579860e5e3bdf7de21b4876b6bf76dce51c0ae
Niek <niek_o@hotmail.com>
2018-11-04 20:10:14 +0000

    redirect to favorites when login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/maps';
+    protected $redirectTo = '/favorites';
 
     /**
      * Create a new controller instance.
