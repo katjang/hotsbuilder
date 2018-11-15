57edf97c4b89770b86949f6730340064165c43c5
Niek Oosterbaan <niek_o@hotmail.com>
2018-11-15 11:49:50 +0100

    fixed missing images in listview

--- resources/views/partials/map/_icon.blade.php
+++ resources/views/partials/map/_icon.blade.php
@@ -1,6 +1,6 @@
 <div class="map icon" data-id="{{$map->id}}">
     <div class="content">
-        <img src="{{asset('img/maps/'. str_replace(' ', '-', $map->name).'.jpg')}}" alt="{{$map->name}}">
+        <img src="{{$map->image}}" alt="{{$map->name}}">
         <div class="tooltip-description">
             <h4>{{$map->name}}</h4>
         </div>
