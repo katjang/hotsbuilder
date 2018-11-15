696f73197b38195952174b7f30c18831e0b6ff3f
Niek Oosterbaan <niek_o@hotmail.com>
2018-11-16 00:30:29 +0100

    fixed maps in build show too

--- resources/views/partials/map/_compact.blade.php
+++ resources/views/partials/map/_compact.blade.php
@@ -1,7 +1,7 @@
 <div class="map compact" data-id="{{$map->id}}">
     <div class="content">
         <a href="{{route('builds', ['map' => $map->id])}}">
-            <img src="{{asset('img/maps/'. str_replace(' ', '-', $map->name).'.jpg')}}" alt="{{$map->name}}">
+            <img src="{{$map->image}}" alt="{{$map->name}}">
         </a>
     </div>
 </div>
